3f91c2b5d57d97739ee7c287479d66e30d37a95c
Hammad Younas <m.hammadyounas@outlook.com>
2024-06-20 18:08:30 +0500

    upload image work done

--- app.py
+++ app.py
@@ -1,60 +1,99 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 import pymongo
+from gridfs import GridFS
 from bson.objectid import ObjectId
+from io import BytesIO
 
 app = Flask(__name__)
 
 # MongoDB Configuration
 # app.config["MONGO_URI"] = "mongodb://localhost:27017/bike_marketplace"
 # mongo = pymongo(app)
-client = pymongo.MongoClient("mongodb+srv://hammadyounas:hammadyounas@testcluster.bi2khpc.mongodb.net/")
+client = pymongo.MongoClient("mongodb://localhost:27017/bike_marketplace")
 db = client["bike_marketplace"]
 sellers = db["sellers"]
 buyers = db["buyers"]
 bikes = db["bikes"]
-
+fs = GridFS(db)
 
 # Seller Route: Submit Bike Information
 @app.route('/sell/bike', methods=['POST'])
 def sell_bike():
-    data = request.json
-    bike_id = bikes.insert_one({
-        "title": data['title'],
-        "model": data['model'],
-        "engine": data['engine'],
-        "registered_in": data['registered_in'],
-        "purchased_year": data['purchased_year'],
-        "petrol_capacity_per_litre": data['petrol_capacity_per_litre'],
-        "total_mileage": data['total_mileage'],
-        "location": data['location'],
-        "selling_price": data['selling_price'],
-        "description": data['description'],
-        "images": data['images'],
+    # Parse non-file form data
+    title = request.form['title']
+    model = request.form['model']
+    engine = request.form['engine']
+    registered_in = request.form['registered_in']
+    purchased_year = request.form['purchased_year']
+    petrol_capacity_per_litre = request.form['petrol_capacity_per_litre']
+    total_mileage = request.form['total_mileage']
+    location = request.form['location']
+    selling_price = request.form['selling_price']
+    description = request.form['description']
+    name = request.form['name']
+    mobile_info = request.form['mobile_info']
+    email = request.form['email']
+    bike_condition = request.form['condition']  # New field to indicate if the bike is new or used
+
+    # Handle image uploads
+    image_ids = []
+    if 'images' in request.files:
+        for image in request.files.getlist('images'):
+            image_id = fs.put(image, content_type=image.content_type, filename=image.filename)
+            image_ids.append(image_id)
+
+    bike_id = db.bikes.insert_one({
+        "title": title,
+        "model": model,
+        "engine": engine,
+        "registered_in": registered_in,
+        "purchased_year": purchased_year,
+        "petrol_capacity_per_litre": petrol_capacity_per_litre,
+        "total_mileage": total_mileage,
+        "location": location,
+        "selling_price": selling_price,
+        "description": description,
+        "images": image_ids,
         "contact": {
-            "name": data['name'],
-            "mobile_info": data['mobile_info'],
-            "email": data['email']
+            "name": name,
+            "mobile_info": mobile_info,
+            "email": email
         },
-        "approved": False
+        "approved": False,
+        "condition": bike_condition  # Store the condition of the bike
     })
+
     return jsonify({"message": "Bike submitted successfully!", "bike_id": str(bike_id.inserted_id)})
 
 # Buyer Route: View All Bikes
 @app.route('/bikes', methods=['GET'])
 def get_bikes():
-    all_bikes = bikes.find({"approved": True})
+    all_bikes = db.bikes.find({"approved": True})
     bike_list = []
     for bike in all_bikes:
         bike['_id'] = str(bike['_id'])
+        bike['images'] = [str(image_id) for image_id in bike.get('images', [])]
+        bike_list.append(bike)
+    return jsonify(bike_list)
+
+# Buyer Route: View Unapproved Bikes (new endpoint)
+@app.route('/bikes/unapproved', methods=['GET'])
+def get_unapproved_bikes():
+    unapproved_bikes = db.bikes.find({"approved": False})
+    bike_list = []
+    for bike in unapproved_bikes:
+        bike['_id'] = str(bike['_id'])
+        bike['images'] = [str(image_id) for image_id in bike.get('images', [])]
         bike_list.append(bike)
     return jsonify(bike_list)
 
 # Buyer Route: View Bike Details
 @app.route('/bike/<bike_id>', methods=['GET'])
 def get_bike(bike_id):
-    bike = bikes.find_one({"_id": ObjectId(bike_id), "approved": True})
+    bike = db.bikes.find_one({"_id": ObjectId(bike_id), "approved": True})
     if bike:
         bike['_id'] = str(bike['_id'])
+        bike['images'] = [str(image_id) for image_id in bike.get('images', [])]
         return jsonify(bike)
     else:
         return jsonify({"error": "Bike not found"}), 404
@@ -63,7 +102,7 @@ def get_bike(bike_id):
 @app.route('/buy', methods=['POST'])
 def buy_bike():
     data = request.json
-    buyer_id = buyers.insert_one({
+    buyer_id = db.buyers.insert_one({
         "name": data['name'],
         "email": data['email'],
         "phone_number": data['phone_number'],
@@ -77,7 +116,7 @@ def buy_bike():
 # Admin Route: Approve Bike Listing
 @app.route('/admin/approve/<bike_id>', methods=['PATCH'])
 def approve_bike(bike_id):
-    result = bikes.update_one({"_id": ObjectId(bike_id)}, {"$set": {"approved": True}})
+    result = db.bikes.update_one({"_id": ObjectId(bike_id)}, {"$set": {"approved": True}})
     if result.modified_count:
         return jsonify({"message": "Bike approved successfully!"})
     else:
@@ -86,7 +125,7 @@ def approve_bike(bike_id):
 # Admin Route: View All Inquiries
 @app.route('/admin/inquiries', methods=['GET'])
 def get_inquiries():
-    all_inquiries = buyers.find()
+    all_inquiries = db.buyers.find()
     inquiry_list = []
     for inquiry in all_inquiries:
         inquiry['_id'] = str(inquiry['_id'])
@@ -98,11 +137,21 @@ def get_inquiries():
 @app.route('/admin/inquiry/<inquiry_id>', methods=['PATCH'])
 def update_inquiry_status(inquiry_id):
     data = request.json
-    result = buyers.update_one({"_id": ObjectId(inquiry_id)}, {"$set": {"status": data['status']}})
+    result = db.buyers.update_one({"_id": ObjectId(inquiry_id)}, {"$set": {"status": data['status']}})
     if result.modified_count:
         return jsonify({"message": "Inquiry status updated successfully!"})
     else:
         return jsonify({"error": "Inquiry not found"}), 404
+
+# New Route: Access Image by ID
+@app.route('/image/<image_id>', methods=['GET'])
+def get_image(image_id):
+    try:
+        print('check')
+        image = fs.get(ObjectId(image_id))
+        return send_file(BytesIO(image.read()), mimetype=image.content_type, as_attachment=False, download_name=image.filename)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
